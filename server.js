b21155bc9b3f492105a4b4e41181c07b5178eec1
Aryeh Harris <harryttd@gmail.com>
2017-01-21 23:57:12 -0500

    Add some card style.

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ module.exports = app
   .use(bodyParser.urlencoded({ extended: true }), bodyParser.json(), require('volleyball'), express.static(resolve(__dirname, 'public')))
   .use('/api', require('./api'))
   // Send index.html for anything else.
-  .use('/node_modules', express.static(resolve(__dirname, 'node_modules')))
   .get('/*', (_, res) => res.sendFile(resolve(__dirname, 'public', 'index.html')));
 
 const server = app.listen(process.env.PORT || 3000, () => {
